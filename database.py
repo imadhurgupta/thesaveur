fd01ec81b4978558ab41aefeabd7abbde520a529
Madhur Gupta <madhurguptaofficial@gmail.com>
2026-08-25 21:06:01 +0530

    M1.9

--- database.py
+++ database.py
@@ -21,8 +21,9 @@ def generate_product_id():
 
 
 def get_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH, timeout=30.0)
     conn.execute("PRAGMA foreign_keys = ON")
+    conn.execute("PRAGMA journal_mode = WAL")
     conn.row_factory = sqlite3.Row
     return conn
 
